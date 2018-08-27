111836c0cd2949a323290e6e2942c272160ecff7
Dusicaa <32860590+Dusicaa@users.noreply.github.com>
2018-08-27 15:42:04 +0200

    Add files via upload

---
+++ src/app/products/product-data.ts
@@ -0,0 +1,18 @@
+import { Product } from './product';
+
+export const PRODUCT_ITEMS: Product[] = [{
+    id: 1,
+    name: 'Ukrasi',
+    description: 'Rucna izrada nakita',
+    price: 4.99
+  }, {
+    id: 2,
+    name: 'Ikone',
+    description: 'Izrada po poruzbini',
+    price: 10.99
+  }, {
+    id: 3,
+    name: 'Slika',
+    description: 'Veliki izbor slika uljana tehnika',
+    price: 5.99
+  }]
